293d17858e762855478fe1476697e2d0da030372
david-mears <kieukmail@gmail.com>
2019-01-23 19:58:36 +0000

    Initial commit, with functioning form and test for index.erb

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra/base'
+
+class Battle < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    player_1_name = params['Player 1']
+    player_2_name = params['Player 2']
+    erb :play, { locals: { player_1_name: player_1_name, player_2_name: player_2_name } }
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
---
+++ spec/features/index_spec.rb
@@ -0,0 +1,10 @@
+feature 'accepts two player names and prints them to the screen' do
+  scenario do
+    visit ('/')
+    fill_in('Player 1', with: 'John Doe')
+    fill_in('Player 2', with: 'Jane Smith')
+    click_button('Submit')
+    expect(page).to have_content 'John Doe'
+    expect(page).to have_content 'Jane Smith'
+  end
+end
