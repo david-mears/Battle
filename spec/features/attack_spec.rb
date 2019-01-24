c698aadef5efd93d9e5fac75e543d3ff3d69c2e4
david-mears <kieukmail@gmail.com>
2019-01-24 14:44:40 +0000

    Displays current scores on play page; improved tests

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,13 +1,13 @@
 feature 'attacks player 2' do
   scenario do
     sign_in_and_play
-    click_button 'Attack'
+    click_button 'Attack Jane Smith'
     expect(page).to have_content 'John Doe attacks Jane Smith!'
   end
   scenario 'player 2 loses ten points' do
     sign_in_and_play
     before_health = $player_2.hp
-    click_button 'Attack'
+    click_button 'Attack Jane Smith'
     expect($player_2.hp).to be < before_health
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,6 +1,6 @@
-feature 'prints the current score of player 2' do
+feature 'prints the current hp of player 2' do
   scenario do
     sign_in_and_play
-    expect(page).to have_content('Jane Smith: 17 HP')
+    expect(page).to have_content($player_2.hp)
   end
 end
