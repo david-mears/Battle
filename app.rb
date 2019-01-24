464c4b9733e082a9069ee32f33ba5d28d08c6813
david-mears <kieukmail@gmail.com>
2019-01-24 10:44:07 +0000

    Implemented post-redirect loop.

--- app.rb
+++ app.rb
@@ -2,15 +2,20 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    player_1_name = params['Player 1']
-    player_2_name = params['Player 2']
-    p params
-    erb :play, { locals: { player_1_name: player_1_name, player_2_name: player_2_name } }
+    session[:player_1_name] = params['Player 1']
+    session[:player_2_name] = params['Player 2']
+    redirect 'play'
+  end
+
+  get '/play' do
+    erb :play, { locals: { player_1_name: session['player_1_name'], player_2_name: session['player_2_name'] } }
   end
 
   # start the server if ruby file executed directly
